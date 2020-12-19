d9e12295ef2da232fdbb53b610f69ee13012c47b
Jameslo599 <jameslo599@gmail.com>
2020-12-19 00:41:13 -0600

    added basic mathmatical functions for calculator

---
+++ script.js
@@ -0,0 +1,14 @@
+let num1 = "";
+let num2 = "";
+let add = function(num1, num2) {
+    return num1 + num2;
+}
+let subtract = function(num1, num2) {
+    return num1 - num2;
+}
+let multiply = function(num1, num2) {
+    return num1 * num2;
+}
+let divide = function(num1, num2) {
+    return num1 / num2;
+}
