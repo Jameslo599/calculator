542df45df9a50cc56205178925599325128bc056
Jameslo599 <jameslo599@gmail.com>
2020-12-24 02:29:22 -0600

    created function to convert displayValue array into number for calculations

--- script.js
+++ script.js
@@ -30,54 +30,68 @@ function operate(operator, num1, num2) {
 }
 
 let display = document.getElementById("display");
-
+let displayValue = [];
 let zero = document.getElementById("zero");
 zero.addEventListener("click", () => {
-    let displayValue = document.createTextNode(0);
-    display.appendChild(displayValue);
+    let input = document.createTextNode(0);
+    display.appendChild(input);
+    displayValue.push(0)
 });
 let one = document.getElementById("one");
 one.addEventListener("click", () => {
-    let displayValue = document.createTextNode(1);
-    display.appendChild(displayValue);
+    let input = document.createTextNode(1);
+    display.appendChild(input);
+    displayValue.push(1);
 });
 let two = document.getElementById("two");
 two.addEventListener("click", () => {
-    let displayValue = document.createTextNode(2);
-    display.appendChild(displayValue);
+    let input = document.createTextNode(2);
+    display.appendChild(input);
+    displayValue.push(2)
 });
 let three = document.getElementById("three");
 three.addEventListener("click", () => {
-    let displayValue = document.createTextNode(3);
-    display.appendChild(displayValue);
+    let input = document.createTextNode(3);
+    display.appendChild(input);
+    displayValue.push(3)
 });
 let four = document.getElementById("four");
 four.addEventListener("click", () => {
-    let displayValue = document.createTextNode(4);
-    display.appendChild(displayValue);
+    let input = document.createTextNode(4);
+    display.appendChild(input);
+    displayValue.push(4)
 });
 let five = document.getElementById("five");
 five.addEventListener("click", () => {
-    let displayValue = document.createTextNode(5);
-    display.appendChild(displayValue);
+    let input = document.createTextNode(5);
+    display.appendChild(input);
+    displayValue.push(5)
 });
 let six = document.getElementById("six");
 six.addEventListener("click", () => {
-    let displayValue = document.createTextNode(6);
-    display.appendChild(displayValue);
+    let input = document.createTextNode(6);
+    display.appendChild(input);
+    displayValue.push(6)
 });
 let seven = document.getElementById("seven");
 seven.addEventListener("click", () => {
-    let displayValue = document.createTextNode(7);
-    display.appendChild(displayValue);
+    let input = document.createTextNode(7);
+    display.appendChild(input);
+    displayValue.push(7)
 });
 let eight = document.getElementById("eight");
 eight.addEventListener("click", () => {
-    let displayValue = document.createTextNode(8);
-    display.appendChild(displayValue);
+    let input = document.createTextNode(8);
+    display.appendChild(input);
+    displayValue.push(8)
 });
 let nine = document.getElementById("nine");
 nine.addEventListener("click", () => {
-    let displayValue = document.createTextNode(9);
-    display.appendChild(displayValue);
+    let input = document.createTextNode(9);
+    display.appendChild(input);
+    displayValue.push("9")
 });
+function arrayConversion() {
+    number = Number(displayValue.join(""));
+    return number;
+}
