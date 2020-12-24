a4d0fb702dc3efdecac1ff015cfe209c57a0300e
Jameslo599 <jameslo599@gmail.com>
2020-12-24 01:55:27 -0600

    wrote cleaner code when declaring functions

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 let num1 = "";
 let num2 = "";
 //Add two numbers
-let add = function(num1, num2) {
+function add(num1, num2) {
     return num1 + num2;
 }
 //Subtract two numbers
-let subtract = function(num1, num2) {
+function subtract(num1, num2) {
     return num1 - num2;
 }
 //Multiply two numbers
-let multiply = function(num1, num2) {
+function multiply(num1, num2) {
     return num1 * num2;
 }
 //Divide two numbers
-let divide = function(num1, num2) {
+function divide(num1, num2) {
     return num1 / num2;
 }
 //Function to include all basic mathematical functions
-let operate = function(operator, num1, num2) {
+function operate(operator, num1, num2) {
     if (operator == add) {
         return add(num1, num2);
     } else if (operator == subtract) {
